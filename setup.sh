85583ebbf1728fa29afaed996dc49eff27fa2f7a
Atishay Jain <atishayjain@Atishays-MacBook-Pro.local>
2025-03-07 21:52:00 -0800

    Add setup script for dependencies

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip install --upgrade pip
+pip install streamlit pandas matplotlib nltk numpy
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
